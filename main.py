6c7bbbdb52012046cd03e74a710d301e7ebe4309
aanazarenko <132454219+aanazarenko@users.noreply.github.com>
2023-06-07 15:00:20 +0000

    [fixed] Now input to stat() function works only with 8..16 bits per color channel

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from enum import Enum
+from typing import Literal
 import numpy as np
 
 class ClinearCalculator:
     
-    def calc_Clinear(self, CsRGB):
+    def calc_Clinear(self, CsRGB) -> float:
         pass
 
     def get_name(self):
@@ -11,7 +13,7 @@ class ClinearCalculator:
 
 class ICCv2(ClinearCalculator):
 
-    def calc_Clinear(self, CsRGB):
+    def calc_Clinear(self, CsRGB) -> float:
     
         if CsRGB <= 0.04045:
             return CsRGB / 12.92
@@ -21,7 +23,7 @@ class ICCv2(ClinearCalculator):
 
 class ICCv2_precise(ClinearCalculator):
 
-    def calc_Clinear(self, CsRGB):
+    def calc_Clinear(self, CsRGB) -> float:
         
         if CsRGB <= 0.0392857:
             return CsRGB / 12.9232102
@@ -30,14 +32,16 @@ class ICCv2_precise(ClinearCalculator):
 
 
 class ICCv4(ClinearCalculator):
-    def calc_Clinear(self, CsRGB):
+    
+    def calc_Clinear(self, CsRGB) -> float:
+        
         if CsRGB <= 0.04045:
             return 0.0772059 * CsRGB + 0.0025
         else:
             return (0.946879 * CsRGB + 0.0520784) ** 2.4 + 0.0025
 
 
-def calc_Lstar_for_monochrome(Clinear):
+def calc_Lstar_for_monochrome(Clinear) -> float:
 
     R709_TO_XYZ_D65 = np.array([
         [0.4124564, 0.3575761, 0.1804375],
@@ -63,7 +67,10 @@ ICCv4_CALC = ICCv4()
 ALL_CALCS = [ICCv2_CALC, ICCv2_precise_CALC, ICCv4_CALC]
 
 
-def stat(CsRGB_number__ICCv2, CsRGB_number__ICCv4, bit_depth):
+class BitDepth(Enum): _8=8; _9=9; _10=10; _11=11; _12=12; _13=13; _14=14; _15=15; _16=16
+
+
+def stat(CsRGB_number__ICCv2: Literal[0, 65535], CsRGB_number__ICCv4: Literal[0, 65535], bit_depth: BitDepth) -> None:
 
     CsRGB_numbers = {
         ICCv2_CALC: CsRGB_number__ICCv2,
@@ -76,28 +83,28 @@ def stat(CsRGB_number__ICCv2, CsRGB_number__ICCv4, bit_depth):
     Lstars = {}
 
     for calc in ALL_CALCS:
-        CsRGBs[calc] = CsRGB_numbers[calc] / (2 ** bit_depth - 1)
+        CsRGBs[calc] = CsRGB_numbers[calc] / (2 ** bit_depth.value - 1)
         Clinears[calc] = calc.calc_Clinear(CsRGBs[calc])
         Lstars[calc] = calc_Lstar_for_monochrome(Clinears[calc])
 
     for calc in ALL_CALCS:
-        print(f"CsRGB number {CsRGB_numbers[calc]:5d} /{bit_depth:2d} CsRGB in range [0..1]: {CsRGBs[calc]:0.5f} ({calc.get_name()})")
+        print(f"CsRGB number {CsRGB_numbers[calc]:5d} /{bit_depth.value:2d} CsRGB in range [0..1]: {CsRGBs[calc]:0.5f} ({calc.get_name()})")
 
     for calc in ALL_CALCS:
-        print(f"CsRGB number {CsRGB_numbers[calc]:5d} /{bit_depth:2d} Clinear in range [0..1]: {Clinears[calc]:0.5f} ({calc.get_name()})")
+        print(f"CsRGB number {CsRGB_numbers[calc]:5d} /{bit_depth.value:2d} Clinear in range [0..1]: {Clinears[calc]:0.5f} ({calc.get_name()})")
 
     for calc in ALL_CALCS:
-        print(f"CsRGB number {CsRGB_numbers[calc]:5d} /{bit_depth:2d} L* in range [0..100]: {Lstars[calc]:0.2f} ({calc.get_name()})")
+        print(f"CsRGB number {CsRGB_numbers[calc]:5d} /{bit_depth.value:2d} L* in range [0..100]: {Lstars[calc]:0.2f} ({calc.get_name()})")
 
     print()
 
-stat(0, 0, 8)  # BLACK - 8-bit
-stat(0, 0, 16)  # BLACK - 16-bit
-stat(1, 1, 8)  # very dark color - 8-bit
-stat(1, 1, 16)  # very dark color - 16-bit
-stat(2**8 - 2, 2**8 - 2, 8)  # brightness color - 8-bit
-stat(2**16 - 2, 2**16 - 2, 16)  # brightness color - 16-bit
-stat(124, 123, 8)  # 20% Image surround reflectance - 8-bit
-stat(118, 117, 8)  # 18% gray card - 8-bit
-stat(15117, 15037, 15)  # 18% gray card - 15-bit
-stat(30235, 30074, 16)  # 18% gray card - 16-bit
+stat(0, 0, BitDepth._8)  # BLACK - 8-bit
+stat(0, 0, BitDepth._16)  # BLACK - 16-bit
+stat(1, 1, BitDepth._8)  # very dark color - 8-bit
+stat(1, 1, BitDepth._16)  # very dark color - 16-bit
+stat(2**8 - 2, 2**8 - 2, BitDepth._8)  # brightness color - 8-bit
+stat(2**16 - 2, 2**16 - 2, BitDepth._16)  # brightness color - 16-bit
+stat(124, 123, BitDepth._8)  # 20% Image surround reflectance - 8-bit
+stat(118, 117, BitDepth._8)  # 18% gray card - 8-bit
+stat(15117, 15037, BitDepth._15)  # 18% gray card - 15-bit
+stat(30235, 30074, BitDepth._16)  # 18% gray card - 16-bit
